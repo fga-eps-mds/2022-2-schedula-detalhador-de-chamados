c0453b146f6a8934b824add8e53f895a01eb450d
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2023-01-11 00:00:32 -0300

    chore: fix db ssl config

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,12 +15,19 @@ const configService = configuration();
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: configService.database.host,
-      port: 5432,
+      port: configService.database.port,
       username: configService.database.user,
       password: configService.database.pass,
       database: configService.database.db,
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       synchronize: true,
+      ...(process.env.ENVIRONMENT === 'PRODUCTION' && {
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }),
     }),
     ScheduleModule,
     IssueModule,
